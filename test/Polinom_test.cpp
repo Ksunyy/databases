0f8fb2a24d5bb0da4839b097b3c7f4d9c467195d
kodt2 <kodt2005@yandex.ru>
2025-02-20 11:34:31 +0300

    Set up project structure with basic files and foldersЭ

---
+++ test/Polinom_test.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+template<typename T>
+class Polinom {
+private:
+	struct stepeni
+	{
+		int koefficient
+		std::int8_t x;
+		std::int8_t y;
+		std::int8_t z;
+	};
+	std::string name;
+	std::vector<stepeni> polinom;
+public:
+	Polinom();
+	~Polinom();
+	operator+ () {};
+	opertator* () {};
+	operator-() {};
+	operator-() {};
+	operator/(){};
+
+//6 разных баз данных на разных структурах данных, провести исследвания скорости
+//массив: sorted table and unsorted table
+//две хеш таблицы
+// поисковое дерево
+//арифметические операции над полиномами
+//парсер для полиномов
+//интерфейс
+};
+class base {
+	struct e {
+		std::string name;
+		Polinom polinom;
+	};
+private:
+	std::stack<e*> data;
+public:
+	void add();
+	void remove();
+	auto find();
+}
